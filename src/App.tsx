60e29488b08ec73c7f3e24ad815da53b8825f795
ohtaekwon <ohtk1119@gmail.com>
2023-05-19 09:12:41 +0900

    Feat 페이지 라우터, 전역상태 Provider 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AuthProvider } from "context/AuthContext";
 import { CssBaseline } from "@mui/material";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+import AuthRoute from "components/AuthRoute";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 
 function App() {
   const theme = createTheme();
@@ -8,6 +17,28 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/signin"
+                element={
+                  <AuthRoute>
+                    <SignIn />
+                  </AuthRoute>
+                }
+              />
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
